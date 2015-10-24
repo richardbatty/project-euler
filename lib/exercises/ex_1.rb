d8d5aaa67a4236989fb835a7f92b1f9c2ad1c082
Richard Batty <richard.batty@reevoo.com>
2015-10-24 21:10:31 +0100

    exercise 6

--- lib/exercises/ex_1.rb
+++ lib/exercises/ex_1.rb
@@ -60,5 +60,16 @@ module Euler
 
     [problem, answer]
   end
+  def ex_6
+    problem = <<-eos
+      Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    eos
+    sum_of_squares = [*1..100].map{|n| n ** 2}.reduce(&:+)
+    square_of_sum = [*1..100].reduce(&:+) ** 2
+    answer = square_of_sum - sum_of_squares
+
+    [problem, answer]
+  end
+
 
 end
